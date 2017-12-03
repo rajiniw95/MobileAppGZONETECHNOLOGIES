5f6878f9b5f32614fff9b8e8512ced5e5aef4d37
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-12-03 12:03:57 +0530

    php files for login and logcheck

---
+++ php_files/logcheck.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+include('includes/db.php');
+$sql = ' select * from login where user_name= "'.$_GET['username'].'" and password= "'.$_GET['password'].'" and request=1 ';
+$result = $mysqli->query($sql);
+if($row=mysqli_fetch_array($result))
+{
+
+	   $markers[] = array(
+	  'id' => $row['id'],
+      );
+	  echo json_encode($markers);
+}
+else{
+echo "false";
+}
+?>
