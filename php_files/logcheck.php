a355017dfaa7d5f094d5de6acf65a8d530e7c357
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-12-03 12:19:32 +0530

    changes to php files of login

--- php_files/logcheck.php
+++ php_files/logcheck.php
@@ -1,12 +1,13 @@
 <?php
 header('Access-Control-Allow-Origin:*');
-include('includes/db.php');
+include('Connecting_DB.php');
 $sql = ' select * from login where user_name= "'.$_GET['username'].'" and password= "'.$_GET['password'].'" and request=1 ';
 $result = $mysqli->query($sql);
 if($row=mysqli_fetch_array($result))
 {
 
 	   $markers[] = array(
+      'role' => $row['role'],
 	  'id' => $row['id'],
       );
 	  echo json_encode($markers);
--- php_files/login.php
+++ php_files/login.php
@@ -2,18 +2,14 @@
 header('Access-Control-Allow-Origin:*');
 include('Connecting_DB.php');
 
-$sql = ' select * from login where user_name= "'.$_GET['username'].'" and password= "'.$_GET['password'].'" and request=1 ';
-$result = $mysqli->query($sql);
-if($row=mysqli_fetch_array($result))
+$sql = " select role from login where user_name= "'.$_GET['username'].'" ";
+$result = mysql_query($sql,$con);
+if($row=mysql_fetch_array($result,$con))
 {
- 
-	   $markers[] = array(
-	  'id' => $row['id'],
-      );
-	  echo json_encode($markers);
+echo $row[0];
 }
-else
-{
+else{
 echo "false";
 }
 ?>
+
