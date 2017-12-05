3b18eda3a44b93927a58e3951910191655eadd9e
udesh sendanayake <usendanayake@gmail.com>
2017-12-05 21:58:30 +0530

    order

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,90 +1,43 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Http } from '@angular/http';
-import { AlertController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Http} from '@angular/http';
+import {AlertController} from 'ionic-angular';
 
-import { HomePagePage } from '../home-page/home-page';
+import {HomePagePage} from '../home-page/home-page';
 
-@Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
-})
+@Component({selector: 'page-login', templateUrl: 'login.html'})
 export class LoginPage {
-  account: {
+  account : {
     username: string;
     password: string;
-  } = { username: "", password: "" };
-  response: any;
-  constructor(public navCtrl: NavController, public http: Http, public Alert: AlertController) {
-  }
+  } = {
+    username: "",
+    password: ""
+  };
+  response : any;
+  constructor(public navCtrl : NavController, public http : Http, public Alert : AlertController) {}
 
-<<<<<<< HEAD
-  goToHomePage(params){
-    this.http.get('http://localhost/myapp/Login.php?username='+this.account.username).subscribe((response) => {
-      var res = response.json();
-      console.log(res);
-      var d = res.toString();
-      if(d.indexOf(this.account.username) != -1){
-        if(d.indexOf(this.account.password) != -1){
-          console.log('Login Success')
-          localStorage.setItem('Auth_Token', this.account.username)
-          this.navCtrl.push(HomePagePage);
-        }else{
-          console.log('Login failed')
-          let alert = this.Alert.create({
-            title: 'Error',
-           subTitle: 'Password Incorrect',
-=======
   goToHomePage(params) {
-    this.http.get('http://localhost/myapp/Login.php?username=' + this.account.username).subscribe((res) => {
-      var d = res.toString();
-      if (d.indexOf(this.account.username) != -1) {
-        if (d.indexOf(this.account.password) != -1) {
-          localStorage.setItem('auth_token', this.account.username);
-          console.log('Login Success')
-          this.navCtrl.push(HomePagePage);
-        } else {
-          console.log('Login failed')
-          let alert = this.Alert.create({
-            title: 'Error',
-            subTitle: 'Password Incorrect',
->>>>>>> b1ab2117ac4b5bd14a96f6d7dcef347b67d29708
-            buttons: ['OK']
-          });
-          alert.present();
-        }
-<<<<<<< HEAD
-      }else{
-        let alert = this.Alert.create({
-          title: 'Error',
-         subTitle: 'Username Incorrect',
-=======
-      } else {
-        let alert = this.Alert.create({
-          title: 'Error',
-          subTitle: 'Username Incorrect',
->>>>>>> b1ab2117ac4b5bd14a96f6d7dcef347b67d29708
-          buttons: ['OK']
-        });
-        alert.present();
-      }
-<<<<<<< HEAD
-});
-
-
-
-=======
-    });
+    this
+      .http
+      .get('http://localhost:8081/GZone/Login.php?username=' + this.account.username)
+      .subscribe((response) => {
+        console.log(response);
+        var res = response.json();
+        console.log(res);
+        // var d = res.toString(); if(d.indexOf(this.account.username) != -1){
+        // if(d.indexOf(this.account.password) != -1){     console.log('Login Success')
+        //    localStorage.setItem('Auth_Token', this.account.username)
+        // this.navCtrl.push(HomePagePage);   }else{     console.log('Login failed')
+        // let alert = this.Alert.create({       title: 'Error',      subTitle:
+        // 'Password Incorrect',       buttons: ['OK']     });     alert.present();   }
+        // } else {   let alert = this.Alert.create({     title: 'Error',     subTitle:
+        // 'Username Incorrect',     buttons: ['OK']   });   alert.present(); }
+      });
   }
->>>>>>> b1ab2117ac4b5bd14a96f6d7dcef347b67d29708
 
+  // logForm(res){   console.log(res + " "+ res.Password);   var d =
+  // res.toString();   if(d.indexOf('Response with status: 200 OK') != -1){
+  // console.log('We are in mf')     this.navCtrl.push(HomePagePage);   } }
 
-  // logForm(res){
-  //   console.log(res + " "+ res.Password);
-  //   var d = res.toString();
-  //   if(d.indexOf('Response with status: 200 OK') != -1){
-  //     console.log('We are in mf')
-  //     this.navCtrl.push(HomePagePage);
-  //   }
-  // }
 }
--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from "@angular/Http";
 
 @Component({
   selector: 'page-order',
   templateUrl: 'order.html'
 })
 export class OrderPage {
-
-  constructor(public navCtrl: NavController) {
+  user : string;
+  res : any;
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.user= localStorage.getItem('Auth_Token');
+    this.http.get('http://localhost/GZone/order.php?username='+this.user).subscribe(data => {
+      this.res = data.json();
+      console.log(this.res);
+    });
   }
   
 }
