436f5b578e1ff2b44eae05c20a46cb5c100c4302
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-12-06 12:04:39 +0530

    ts of delivery page

--- src/pages/delivery/delivery.ts
+++ src/pages/delivery/delivery.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { Http } from "@angular/Http";
 
 @Component({
   selector: 'page-delivery',
   templateUrl: 'delivery.html'
 })
 export class DeliveryPage {
-
-  constructor(public navCtrl: NavController) {
+  user : string;
+  res : any;
+  public deliveryId;
+  constructor(public navCtrl: NavController, public http: Http, public navparams : NavParams) {
+    this.deliveryId = this.navparams.get("id");
+    this.user= localStorage.getItem('Auth_Token');
+    this.http.get('http://localhost/GZone/delivery.php?id='+this.deliveryId).subscribe(data => {
+      this.res = data.json();
+      console.log(this.res);
+    });
   }
   
 }
