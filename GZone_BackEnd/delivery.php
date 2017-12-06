fd20bb7a401988f3668a0b2f1721a96520a43b88
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-12-06 11:18:43 +0530

    backend php files of previous-purchases,deliverylist and delivery

---
+++ GZone_BackEnd/delivery.php
@@ -0,0 +1,21 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type,x-prototype-version,x-requested-with');
+
+include('Connecting_DB.php');
+//$con= mysql_connect("localhost","root","");
+//$db = mysql_select_db("Hello",$con);
+
+$ID = $_GET['id'];
+$query = "select * from order where id=$ID";
+$result = $link->query($query) or die($link->error . __LINE__);
+$card = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $card[] = $row;
+    }
+}
+$json_response = json_encode($card);
+echo $json_response;
+?>
---
+++ GZone_BackEnd/deliverylist.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type,x-prototype-version,x-requested-with');
+include('Connecting_DB.php');
+//Select Data from the postadvertiesement table in the smartapp database
+$query = "select user_name from login";
+
+$result = $mysqli->query($query) or die($mysqli->error . __LINE__);
+$ban = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $ban[] = $row;
+    }
+}
+$json_response = json_encode($ban);
+echo $json_response;
+?>
