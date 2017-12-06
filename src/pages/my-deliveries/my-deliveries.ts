ca57c421f6714e64b82ab018148b43b16cae7139
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-12-06 12:00:12 +0530

    ts and html files of my-deliveries connected with backend

--- src/pages/my-deliveries/my-deliveries.ts
+++ src/pages/my-deliveries/my-deliveries.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Headers, Response } from '@angular/http';
 
 import { DeliveryPage } from '../delivery/delivery';
 
@@ -7,14 +8,24 @@ import { DeliveryPage } from '../delivery/delivery';
   selector: 'page-my-deliveries',
   templateUrl: 'my-deliveries.html'
 })
+
 export class MyDeliveriesPage {
 
-  constructor(public navCtrl: NavController) {
+  user : string;
+  res : any[];
+  constructor(public navCtrl: NavController, public http: Http) {
+   this.user= localStorage.getItem('Auth_Token');
+    this.http.get('http://localhost:8081/GZone/deliverylist.php?username='+this.user).subscribe(data => {
+      this.res = data.json();
+      console.log(this.res);
+    });
   }
 
-  goToDelivery(params){
+  goToDelivery(params)(params){
     if (!params) params = {};
-    this.navCtrl.push(DeliveryPage);
+    this.navCtrl.push(DeliveryPage, {
+      id : params
+    });
   }
 
 }
