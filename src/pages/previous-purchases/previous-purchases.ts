bcbd665526c4b0574f6cfd61e431f6b318fd8d37
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-12-06 11:52:49 +0530

    ts and html of previous-purchases connected with backend

--- src/pages/previous-purchases/previous-purchases.ts
+++ src/pages/previous-purchases/previous-purchases.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Http, Headers, Response } from '@angular/http';
 import { OrderPage } from '../order/order';
 
 @Component({
   selector: 'page-previous-purchases',
   templateUrl: 'previous-purchases.html'
 })
-export class PreviousPurchasesPage {
 
-  constructor(public navCtrl: NavController) {
+export class PreviousPurchasesPage {
+  user : string;
+  res : any[];
+  constructor(public navCtrl: NavController, public http: Http) {
+   this.user= localStorage.getItem('Auth_Token');
+    this.http.get('http://localhost:8081/GZone/previous-purchases.php?username='+this.user).subscribe(data => {
+      this.res = data.json();
+      console.log(this.res);
+    });
   }
 
   goToOrder(params){
     if (!params) params = {};
-    this.navCtrl.push(OrderPage);
+    this.navCtrl.push(OrderPage, {
+      id : params
+    });
   }
 
 }
