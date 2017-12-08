92f93f665a7b908c643d911d3dc0dcb969cacd4d
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-12-08 23:43:44 +0530

    UI: search

--- GZone_BackEnd/Connecting_DB.php
+++ GZone_BackEnd/Connecting_DB.php
@@ -2,7 +2,7 @@
 	$host = 'localhost:3301';
 	$user = 'root';
 	$pass = '';
-	$db = 'GZoneMobile';
+	$db = 'newapp2';
 	
 	$mysqli = mysqli_connect($host, $user, $pass, $db);
 	if (mysqli_connect_errno()){
